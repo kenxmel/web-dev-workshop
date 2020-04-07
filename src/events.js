e8ba2beea7df1554254906b58de09c4e829ba05b
Kenneth Lu <z5259931@cse.unsw.edu.au>
2020-04-07 21:44:19 +1000

    Added events page

---
+++ src/events.js
@@ -0,0 +1,25 @@
+sports = ["3x3 Basketball", "Archery", "Artistic Gymnastics", "Artistic Swimming", "Athletics", "Badminton", "Baseball / Softball", "Basketball", "Beach Volleyball", "Boxing", "Canoe Slalom", "Canoe Sprint", " Cycling BMX Freestyle", "Cycling BMX Racing"]
+
+const main = document.getElementById("main");
+
+console.log("Hello");
+
+for (let i = 0; i < sports.length; i++) {
+	const sportsContainer = document.createElement("div");
+	sportsContainer.className = "sports-container";
+	
+	const sportsImage = document.createElement("img");
+	sportsImage.src = "../assets/placeholder.png";
+	sportsImage.alt = sports[i];
+	sportsImage.className = "sports-image";
+
+	const sportsName = document.createElement("div");
+	sportsName.className = "sports-name";
+
+	const textNode = document.createTextNode(sports[i]);
+
+	sportsName.appendChild(textNode);
+	sportsContainer.appendChild(sportsImage);
+	sportsContainer.appendChild(sportsName);
+	main.appendChild(sportsContainer);
+}
